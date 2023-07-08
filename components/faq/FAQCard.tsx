80a79d8a60c275722dd4f8d4dc469feef156a0e8
Emmanuel Jolaiya <jolaiyaemmanuel@gmail.com>
2023-07-08 17:14:32 +0100

    Updated FAQ styling on web

--- components/faq/FAQCard.tsx
+++ components/faq/FAQCard.tsx
@@ -9,7 +9,7 @@ const FAQCard = ({ question, answer}: TFAQCard) => {
 
   return (
     <div className=' col-span-1 flex flex-col gap-8'>
-      <div className=' rounded-lg h-[36px] w-[166px] bg-gradient-to-r p-[1px] from-yellow-0 via-purple-30 to-red-0 '>
+      <div className=' rounded-lg h-[36px] w-[166px] laptop:w-[176px] bg-gradient-to-r p-[1px] from-yellow-0 via-purple-30 to-red-0 '>
                 <div className=' bg-[#465263] backdrop-blur-sm flex items-center justify-center text-white w-full h-full rounded-lg'>
                     <span className='laptop:font-p-semibold font-s-medium laptop:text-body text-body-xm'>{question}</span>
                 </div>
