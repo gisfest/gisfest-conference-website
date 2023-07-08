2e8d9beca45ee21d9b30a2f10cb798d305a74aee
jeafreezy <jolaiyaemmanuel@gmail.com>
2023-07-08 17:21:55 +0100

    fixed styling bug in faq card

--- components/faq/FAQCard.tsx
+++ components/faq/FAQCard.tsx
@@ -1,24 +1,23 @@
-import React from 'react'
+import React from 'react';
 
 type TFAQCard = {
 	question: string;
 	answer: string;
 };
 
-const FAQCard = ({ question, answer}: TFAQCard) => {
+const FAQCard = ({ question, answer }: TFAQCard) => {
+	return (
+		<div className=" col-span-1 flex flex-col gap-8">
+			<div className=" rounded-lg h-[36px] w-[166px] laptop:w-[226px]   bg-gradient-to-r p-[1px] from-yellow-0 via-purple-30 to-red-0 ">
+				<div className=" bg-[#465263] backdrop-blur-sm flex items-center justify-center text-white w-full h-full rounded-lg">
+					<span className="laptop:font-p-semibold whitespace-nowrap font-s-medium laptop:text-body text-body-xm">
+						{question}
+					</span>
+				</div>
+			</div>
+			<p className="text-tc-10 text-body">{answer}</p>
+		</div>
+	);
+};
 
-  return (
-    <div className=' col-span-1 flex flex-col gap-8'>
-      <div className=' rounded-lg h-[36px] w-[166px] laptop:w-[176px] bg-gradient-to-r p-[1px] from-yellow-0 via-purple-30 to-red-0 '>
-                <div className=' bg-[#465263] backdrop-blur-sm flex items-center justify-center text-white w-full h-full rounded-lg'>
-                    <span className='laptop:font-p-semibold font-s-medium laptop:text-body text-body-xm'>{question}</span>
-                </div>
-        </div>
-        <p className='text-tc-10 text-body'>
-            {answer}
-        </p>
-    </div>
-  )
-}
-
-export default FAQCard
+export default FAQCard;
